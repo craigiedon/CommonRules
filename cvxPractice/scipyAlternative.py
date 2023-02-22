97c5ff87acbcf2672c3fa40508d0c8f93531828c
Craig Innes <craiginnes@gmail.com>
2023-02-22 23:37:34 +0000

    progress

--- cvxPractice/scipyAlternative.py
+++ cvxPractice/scipyAlternative.py
@@ -3,7 +3,6 @@ from typing import List
 
 import matplotlib.pyplot as plt
 import numpy as np
-import scipy
 import scipy.optimize as spt
 from scipy.optimize import OptimizeResult
 
@@ -65,7 +64,8 @@ class CarAction:
         return len(self.to_array())
 
 @dataclass
-class TaskConstraints:
+class TaskConfig:
+    T: int
     x_goal: float
     y_goal: float
     car_width: float
@@ -76,23 +76,22 @@ class TaskConstraints:
     ang_vel_max: float
     lanes: np.ndarray
 
-def car_mpc(start_state: CarState, task: TaskConstraints, T: float, static_obstacles: List[RectObstacle]) -> OptimizeResult:
+def car_mpc(start_state: CarState, task: TaskConfig, static_obstacles: List[RectObstacle]) -> OptimizeResult:
     s_0 = start_state.to_array()
     state_dims = len(s_0)
     action_dims = CarAction(0.0, 0.0).n_dims()
     lane_dims = len(task.lanes)
 
+    obj_fun = lambda d_vars: interstate_objective(d_vars, start_state, task)
+
     cons = (
-        {"type": "eq", "fun": start_constraints},
-        {"type": "eq", "fun": x_evolution},
-        {"type": "eq", "fun": y_evolution},
-        {"type": "eq", "fun": vel_evolution},
-        {"type": "eq", "fun": heading_evolution},
-        {"type": "eq", "fun": lane_selection_simplex}
+        {"type": "eq", "fun": lambda d_vars: start_constraints(d_vars, task, start_state)},
+        {"type": "eq", "fun": lambda d_vars: state_evolution(d_vars, task)},
+        {"type": "eq", "fun": lambda d_vars: lane_selection_simplex(d_vars, task)}
     )
 
-    lbs = -np.ones((T, state_dims + action_dims + lane_dims)) * np.inf
-    ubs = np.ones((T, state_dims + action_dims + lane_dims)) * np.inf
+    lbs = -np.ones((task.T, state_dims + action_dims + lane_dims)) * np.inf
+    ubs = np.ones((task.T, state_dims + action_dims + lane_dims)) * np.inf
 
     lbs[:, 2] = -task.v_max
     ubs[:, 2] = task.v_max
@@ -110,18 +109,13 @@ def car_mpc(start_state: CarState, task: TaskConstraints, T: float, static_obsta
     ubs[:, 6:9] = 1.0
 
 
-    state_guess = np.random.random((T, state_dims + action_dims + lane_dims)).reshape(-1)
+    state_guess = np.random.random((task.T, state_dims + action_dims + lane_dims)).reshape(-1)
 
     return spt.minimize(obj_fun, state_guess, constraints=cons, bounds=spt.Bounds(lbs.reshape(-1), ubs.reshape(-1)))
 
-res = car_mpc(CarState(0.0, 0.0, 0.0, 0.0), TaskConstraints(700.0, 5.0, 2.0, 1.0, 5.0, 30.0, 1.0, 1.0, np.array([-5.0, 0.0, 5.0])), 20, [])
 
-
-
-
-
-def obj_fun(d_vars: np.ndarray):
-    reshaped_vars = d_vars.reshape(T, state_dims + action_dims + lane_selection_dims)
+def interstate_objective(d_vars: np.ndarray, start_state: CarState, task: TaskConfig):
+    reshaped_vars = d_vars.reshape(task.T, -1)
     xs = reshaped_vars[:, 0]
     ys = reshaped_vars[:, 1]
     vs = reshaped_vars[:, 2]
@@ -130,13 +124,12 @@ def obj_fun(d_vars: np.ndarray):
     ang_vels = reshaped_vars[:, 5]
     lane_selectors = reshaped_vars[:, 6:9]
 
-
     # Distance to destination cost
-    x_progress_cost = (((xs - x_g) / (x_0 - x_g)) ** 2).sum()
-    y_progress_cost = (((ys - y_g) / (y_0 - y_g)) ** 2).sum()
+    x_progress_cost = (((xs - task.x_goal) / (start_state.x - task.x_goal)) ** 2).sum()
+    y_progress_cost = (((ys - task.y_goal) / (start_state.y - task.y_goal)) ** 2).sum()
 
     # Track the reference velocity
-    vel_track_cost = ((vs - v_track) ** 2).sum()
+    vel_track_cost = ((vs - task.v_goal) ** 2).sum()
 
     # Keep velocity low
     acc_cost = (accs ** 2).sum()
@@ -151,96 +144,68 @@ def obj_fun(d_vars: np.ndarray):
     road_align_cost = (hs ** 2).sum()
 
     # Lane alignment
-    chosen_lanes = lane_selectors @ lanes
+    chosen_lanes = lane_selectors @ task.lanes
     lane_align_cost = ((ys - chosen_lanes) ** 2).sum()
-
-
 
     return x_progress_cost + y_progress_cost + vel_track_cost + acc_cost + ang_vel_cost + jerk_cost + road_align_cost + lane_align_cost
 
-def state_evolution(d_vars):
-    reshaped_vars=  d_vars.reshape(T, -1)
+def state_evolution(d_vars, task: TaskConfig):
+    reshaped_vars=  d_vars.reshape(task.T, -1)
     xs = reshaped_vars[:, 0]
     ys = reshaped_vars[:, 1]
     vs = reshaped_vars[:, 2]
     hs = reshaped_vars[:, 3]
-
-    return None
-
-
-def x_evolution(d_vars):
-    reshaped_vars = d_vars.reshape(T, state_dims + action_dims + lane_selection_dims)
-    xs = reshaped_vars[:, 0]
-    vs = reshaped_vars[:, 2]
-    hs = reshaped_vars[:, 3]
-
-    return xs[1:] - (xs[:-1] + np.cos(hs[:-1]) * vs[:-1])
-
-def y_evolution(d_vars):
-    reshaped_vars = d_vars.reshape(T, state_dims + action_dims + lane_selection_dims)
-    ys = reshaped_vars[:, 1]
-    vs = reshaped_vars[:, 2]
-    hs = reshaped_vars[:, 3]
-
-    return ys[1:] - (ys[:-1] + np.sin(hs[:-1]) * vs[:-1])
-
-def vel_evolution(d_vars):
-    reshaped_vars = d_vars.reshape(T, state_dims + action_dims + lane_selection_dims)
-    vs = reshaped_vars[:, 2]
     accs = reshaped_vars[:, 4]
-
-    return vs[1:] - (vs[:-1] + accs[:-1])
-
-def heading_evolution(d_vars):
-    reshaped_vars = d_vars.reshape(T, state_dims + action_dims + lane_selection_dims)
-    hs = reshaped_vars[:, 3]
     ang_vs = reshaped_vars[:, 5]
-    return hs[1:] - (hs[:-1] + ang_vs[:-1])
+
+    return np.concatenate((
+        xs[1:] - (xs[:-1] + np.cos(hs[:-1]) * vs[:-1]),
+        ys[1:] - (ys[:-1] + np.sin(hs[:-1]) * vs[:-1]),
+        vs[1:] - (vs[:-1] + accs[:-1]),
+        hs[1:] - (hs[:-1] + ang_vs[:-1])
+    ))
 
 
-def start_constraints(d_vars):
-    reshaped_vars = d_vars.reshape(T, state_dims + action_dims + lane_selection_dims)
-    xs = reshaped_vars[:, 0]
-    ys = reshaped_vars[:, 1]
-    vs = reshaped_vars[:, 2]
-    hs = reshaped_vars[:, 3]
+def start_constraints(d_vars, task: TaskConfig, start_state: CarState):
+    reshaped_vars = d_vars.reshape(task.T, -1)
+    state_starts = reshaped_vars[0, 0:start_state.n_dims()]
 
-    state_starts = reshaped_vars[0, 0:state_dims]
+    return state_starts - start_state.to_array()
 
-    return state_starts - s_0
 
-def lane_selection_simplex(d_vars):
-    reshaped_vars = d_vars.reshape(T, state_dims + action_dims + lane_selection_dims)
+def lane_selection_simplex(d_vars, task: TaskConfig):
+    reshaped_vars = d_vars.reshape(task.T, -1)
     lane_selectors = reshaped_vars[:, 6:9]
     lane_sums = np.sum(lane_selectors, axis=1)
     return lane_sums - 1.0
 
-res_vals = res.x.reshape(T, state_dims + action_dims + lane_selection_dims)
-# print("Result vals: ", res_vals)
+task_config = TaskConfig(20, 700.0, 5.0, 2.0, 1.0, 5.0, 30.0, 1.0, 1.0, np.array([-5.0, 0.0, 5.0]))
+res = car_mpc(CarState(0.0, 0.0, 0.0, 0.0), task_config, [])
+res_vals = res.x.reshape(task_config.T, -1)
+
 print("xs:", res_vals[:, 0])
 print("ys:", res_vals[:, 1])
 
 n_subplots = 4
 
 plt.subplot(n_subplots, 1, 1)
-plt.plot(range(T), res_vals[:, 0])
+plt.plot(range(task_config.T), res_vals[:, 0])
 plt.xlabel("t")
 plt.ylabel("X")
 
 plt.subplot(n_subplots, 1, 2)
-plt.plot(range(T), res_vals[:, 1])
+plt.plot(range(task_config.T), res_vals[:, 1])
 plt.ylim(-5, 5)
 plt.xlabel("t")
 plt.ylabel("Y")
 
 plt.subplot(n_subplots, 1, 3)
-plt.plot(range(T), res_vals[:, 2])
-# plt.ylim(-np.pi, np.pi)
+plt.plot(range(task_config.T), res_vals[:, 2])
 plt.xlabel("t")
 plt.ylabel("Velocity")
 
 plt.subplot(n_subplots, 1, 4)
-plt.plot(range(T), res_vals[:, 3])
+plt.plot(range(task_config.T), res_vals[:, 3])
 plt.ylim(-np.pi, np.pi)
 plt.xlabel("t")
 plt.ylabel("$\\theta$")
