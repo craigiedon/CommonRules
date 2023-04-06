36f19a14547f382d383fbec61194e295b8446f31
Craig Innes <craiginnes@gmail.com>
2023-04-07 00:26:39 +0100

    More experiments and some visualizations

--- PlotSalientData.py
+++ PlotSalientData.py
@@ -6,7 +6,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.data import DataLoader, IterableDataset, TensorDataset
 from sklearn.model_selection import StratifiedShuffleSplit
-from sklearn.metrics import roc_curve
+from sklearn.metrics import roc_curve, roc_auc_score
 from torchvision.ops import sigmoid_focal_loss
 
 
@@ -62,10 +62,79 @@ def plot_roc(v_data, model, model_name):
     with torch.no_grad():
         final_val_preds = torch.sigmoid(model(v_data.tensors[0]))
     fpr, tpr, threshs = roc_curve(v_data.tensors[1].cpu(), final_val_preds.cpu(), pos_label=1)
+    print(f"ROC AUC: {model_name} --- {roc_auc_score(v_data.tensors[1].cpu(), final_val_preds.cpu())}")
     plt.plot(fpr, tpr, label=model_name)
     plt.xlabel("False Positive Rate")
     plt.ylabel("True Positive Rate")
     plt.plot(np.linspace(0, 1), np.linspace(0, 1), '--', color='black', alpha=0.4)
+
+def train_model(train_loader, val_loader, pem, loss_fn, epochs) -> nn.Module:
+    avg_train_losses = []
+    avg_val_losses = []
+
+    optim = torch.optim.Adam(pem.parameters())
+
+    for i in range(epochs):
+
+        pem.train()
+        train_losses = []
+        # guess_one_losses = []
+        # guess_mu_losses = []
+        # guess_om_losses = []
+        for x, label in train_loader:
+            pred = pem(x)
+            loss = loss_fn(pred, label.unsqueeze(1))
+
+            # pred_nans = pred[torch.isnan(pred)]
+            # x_nans = x[torch.isnan(x)]
+
+            # g1_loss = pure_loss_fn(torch.ones_like(pred), label.unsqueeze(1))
+            # g_mu_loss = pure_loss_fn(torch.full_like(pred, g_mu), label.unsqueeze(1))
+
+            # g_om = guess_mu_by_occ(x.detach(), occ_mus.detach()).detach()
+            # F.binary_cross_entropy(g_om, label.unsqueeze(1).detach())
+            # g_om_loss = F.binary_cross_entropy(g_om, label.unsqueeze(1).detach())
+            # with torch.no_grad():
+            #     g_om_loss = F.binary_cross_entropy(g_om, torch.ones_like(g_om))
+            # g_om_loss = pure_loss_fn(g_om, label.unsqueeze(1).detach())
+
+            optim.zero_grad()
+            loss.backward()
+            optim.step()
+
+            train_losses.append(loss.item())
+            # guess_one_losses.append(g1_loss.item())
+            # guess_mu_losses.append(g_mu_loss.item())
+            # guess_om_losses.append(g_om_loss.item())
+
+        avg_train_loss = np.mean(train_losses)
+        # avg_g1_loss = np.mean(guess_one_losses)
+        # avg_gmu_loss = np.mean(guess_mu_losses)
+        # avg_gom_loss = np.mean(guess_om_losses)
+
+        pem.eval()
+        val_losses = []
+        for x, label in val_loader:
+            with torch.no_grad():
+                pred = pem(x)
+                val_loss = loss_fn(pred, label.unsqueeze(1))
+                val_losses.append(val_loss.item())
+
+        avg_val_loss = np.mean(val_losses)
+
+        print(f"{i}: Avg Loss T - {avg_train_loss} \t V - {avg_val_loss}")
+        avg_train_losses.append(avg_train_loss)
+        avg_val_losses.append(avg_val_loss)
+
+    plt.plot(range(epochs), avg_train_losses, label='train')
+    plt.plot(range(epochs), avg_val_losses, label='val')
+    plt.show()
+
+        # if i % checkpoint_interval == 0:
+        #     plot_roc(val_data, pem, i)
+
+
+    return pem
 
 
 def run():
@@ -86,12 +155,12 @@ def run():
     s_rys = s_inp[:, 2]
     s_lens = s_inp[:, 3]
     s_ws = s_inp[:, 4]
-    X = np.stack((s_xs, s_zs, s_rys, s_lens, s_ws), axis=1)
-    # X = np.stack((s_xs, s_zs, s_rys), axis=1)
-    X = torch.from_numpy(X).to(dtype=torch.float)
+    # X = np.stack((s_xs, s_zs, s_rys, s_lens, s_ws), axis=1)
+    X = np.stack((s_xs, s_zs, s_rys), axis=1)
 
-    mus = X.mean(dim=0)
-    stds = X.std(dim=0)
+    X = torch.from_numpy(X).to(dtype=torch.float)
+    mu = X.mean(dim=0)
+    std = X.std(dim=0)
     X = (X - X.mean(dim=0)) / X.std(dim=0)
 
     s_occs = torch.from_numpy(s_inp[:, 5]).to(dtype=torch.long)
@@ -105,117 +174,62 @@ def run():
     s_det = torch.from_numpy(s_det).to(dtype=torch.float)
     g_mu = torch.mean(s_det)
 
-    # plt.hist2d(s_xs, s_zs, bins=30)
-    # plt.colorbar()
-    # plt.show()
-
-    # clf = MLPClassifier(hidden_layer_sizes=(5, 2), alpha=1e-5, random_state=1)
-    # clf.fit(X, s_det)
-
-    pem = SimpleClassPEM(X.shape[1], 128).cuda()
-    # pem = nn.Linear(X.shape[1], 1).cuda()
-    # Loss
-    # loss_fn = nn.BCEWithLogitsLoss()
-    loss_fn = lambda inps, targs: sigmoid_focal_loss(inps, targs, reduction='mean', alpha=0.6)
-
-    pure_loss_fn = nn.BCELoss()
-    optim = torch.optim.Adam(pem.parameters())
-    # optim = torch.optim.SGD(pem.parameters(), lr=1e-2)
-
     train_idx, val_idx = next(StratifiedShuffleSplit(n_splits=1, random_state=1).split(X, s_det))
     X = X.cuda()
     s_det = s_det.cuda()
+
     train_data = TensorDataset(X[train_idx], s_det[train_idx])
     val_data = TensorDataset(X[val_idx], s_det[val_idx])
 
     train_loader = DataLoader(train_data, batch_size=1024, shuffle=True)
     val_loader = DataLoader(val_data, batch_size=1024, shuffle=False)
 
+    bce_logit_loss_fn = nn.BCEWithLogitsLoss()
+    focal_loss_fn = lambda inps, targs: sigmoid_focal_loss(inps, targs, reduction='mean', alpha=0.6)
+
     # Batches from dataloader
-    epochs = 250
-    # epochs = 10
-    # checkpoint_interval = 100
-    # fig, axs = plt.subplots(1,2)
-    # loss_ax, roc_ax = axs
+    epochs = [250]
+    capacities = [128]
+    loss_fns = [bce_logit_loss_fn]
 
-    avg_train_losses = []
-    avg_val_losses = []
+    configs = list(itertools.product(capacities, epochs, loss_fns))
+    pems = [SimpleClassPEM(X.shape[1], conf[0]).cuda() for conf in configs]
 
-    for i in range(epochs):
+    trained_models = []
+    for (c, e, l_fn), pm in zip(configs, pems):
+        trained_models.append(train_model(train_loader, val_loader, pm, l_fn, e))
 
-        pem.train()
-        train_losses = []
-        guess_one_losses = []
-        guess_mu_losses = []
-        guess_om_losses = []
-        for x, label in train_loader:
-            pred = pem(x)
-            loss = loss_fn(pred, label.unsqueeze(1))
+    for (c, e, l_fn), pm in zip(configs, pems):
+        plot_roc(val_loader.dataset, pm, f"C:{c} E:{e} L:{l_fn}")
 
-            # pred_nans = pred[torch.isnan(pred)]
-            # x_nans = x[torch.isnan(x)]
 
-            g1_loss = pure_loss_fn(torch.ones_like(pred), label.unsqueeze(1))
-            g_mu_loss = pure_loss_fn(torch.full_like(pred, g_mu), label.unsqueeze(1))
-
-            # g_om = guess_mu_by_occ(x.detach(), occ_mus.detach()).detach()
-            # F.binary_cross_entropy(g_om, label.unsqueeze(1).detach())
-            # g_om_loss = F.binary_cross_entropy(g_om, label.unsqueeze(1).detach())
-            # with torch.no_grad():
-            #     g_om_loss = F.binary_cross_entropy(g_om, torch.ones_like(g_om))
-            # g_om_loss = pure_loss_fn(g_om, label.unsqueeze(1).detach())
-
-            optim.zero_grad()
-            loss.backward()
-            optim.step()
-
-            train_losses.append(loss.item())
-            guess_one_losses.append(g1_loss.item())
-            guess_mu_losses.append(g_mu_loss.item())
-            # guess_om_losses.append(g_om_loss.item())
-
-        avg_train_loss = np.mean(train_losses)
-        avg_g1_loss = np.mean(guess_one_losses)
-        avg_gmu_loss = np.mean(guess_mu_losses)
-        # avg_gom_loss = np.mean(guess_om_losses)
-
-        pem.eval()
-        val_losses = []
-        for x, label in val_loader:
-            with torch.no_grad():
-                pred = pem(x)
-                val_loss = loss_fn(pred, label.unsqueeze(1))
-                val_losses.append(val_loss.item())
-
-        avg_val_loss = np.mean(val_losses)
-
-        print(f"{i}: Avg Loss T - {avg_train_loss} \t V - {avg_val_loss} \t G_1 - {avg_g1_loss} \t G_mu {avg_gmu_loss}")
-        avg_train_losses.append(avg_train_loss)
-        avg_val_losses.append(avg_val_loss)
-
-        # if i % checkpoint_interval == 0:
-        #     plot_roc(val_data, pem, i)
-
-    plot_roc(val_data, pem, epochs)
-    # plot_roc(val_data, lambda x: torch.full((x.shape[0], 1), torch.logit(g_mu)), "g_mu")
     plt.legend()
     plt.show()
 
-    plt.plot(range(epochs), avg_train_losses, label='train')
-    plt.plot(range(epochs), avg_val_losses, label='val')
-    plt.legend()
+    num_x, num_y = 50, 50
+    ps = torch.stack(torch.meshgrid([torch.linspace(-100, 100, num_x), torch.linspace(-100, 100, num_y)]), dim=2).reshape(-1, 2)
+    ps = (ps - mu[0:2]) / std[0:2]
+
+    ev_rot = torch.full((len(ps),), -np.pi / 2.0) # -np.pi / 2.0)
+    ev_rot = (ev_rot - mu[2]) / std[2]
+    ev_occ = F.one_hot(torch.tensor(2), num_classes=3).repeat(len(ps), 1)
+
+    eval_xs = torch.column_stack((ps, ev_rot, ev_occ))
+    m = pems[0]
+
+    # TODO: Oh shit --- i am an idiot: You need to *Normalize* here!!!
+    z_preds = torch.sigmoid(m(eval_xs.cuda())).cpu().detach().reshape(num_x, num_y)
+
+    ps = ps.reshape(num_x, num_y, 2)
+
+
+    plt.pcolormesh(ps[:,:, 0], ps[:,:, 1], z_preds)
+    plt.colorbar()
     plt.show()
 
-    # p_xs = np.linspace(-40, 40)
-    # p_ys = np.linspace(0, 40)
-    # ev = np.array(list(itertools.product(p_xs, p_ys)))
+    # TODO: Hmm...so, can I do some analysis? The analysis I would like to do is...meshgrid up the whole x-y space, split by occlusion, and interpolate between?
 
-    # preds = clf.predict(ev)
-    # ranges = (-40, 40), (0, 80)
 
-    # Load in the salient data
-    # Stick x/ys on some sort of histogram
-    # Display
 
 
 if __name__ == "__main__":
