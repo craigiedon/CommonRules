f2978f860d608ec9765b5b71762f43a4ea6dfba4
Craig Innes <craiginnes@gmail.com>
2023-12-01 17:55:21 +0000

    Cleaup for submission

--- domPointExplanation.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-from scipy.stats import multivariate_normal
-
-g1 = multivariate_normal(mean=[0, 0])
-g2 = multivariate_normal([0, 0])
-
-plt.pcolormesh()
